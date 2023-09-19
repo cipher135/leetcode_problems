6d1cce3ae36cb97b036f1e269f0199f23639e947
Santosh Sahni <meet.santosh96@gmail.com>
2023-09-19 09:51:19 +0530

    https://leetcode.com/problems/same-tree/

--- Solution/src/sol/main/SolveHere.java
+++ Solution/src/sol/main/SolveHere.java
@@ -241,4 +241,13 @@ public class SolveHere {
             inOrder(root.right);
     }
     
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+    	if(p==null && q==null)
+    		return true;
+    	if(p==null || q==null)
+    		return false;
+    	if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    	return false;
+    }
 }
